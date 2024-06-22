49a52ef35ca7c42fa209f0338f609727c344e7d9
leggiero <vietthanh2005@gmail.com>
2024-06-22 18:10:59 +0700

    change the code of the counter

--- source/main.py
+++ source/main.py
@@ -1,31 +1,11 @@
 import time
 
 
-class OPS:
+# Count the number of basic operations in 1 second
+start_time = time.time()
+count = 0
 
-    def __init__(self):
-        self.seconds = {}
-        pass
+while time.time() - start_time < 1:
+    count += 1
 
-    def add(self):
-        sec = int(round(time.time()))
-
-        if sec in self.seconds:
-            self.seconds[sec] = self.seconds[sec]+1
-        else:
-            self.seconds[sec] = 1
-
-    def print(self):
-        print(self.seconds)
-
-
-o = OPS()
-i = 0
-# try for 20,000,000 basic operators
-while i < 20000000:
-    i = i + 1
-    o.add()
-
-o.print()
-# prints {1544912424: 769, 1544912425: 2648, 1544912426: 2578, 1544912427: 2599, 1544912428: 1406}
-# the result shows, the number of seconds counted from epoch, and the number of operators in a second.
+print(f"Number of basic operations in 1 second: ", count)
